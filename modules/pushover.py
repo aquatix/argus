fb63b49302560d4ab139c3749347edf5878a18c9
Michiel Scholten <michiel@diginaut.net>
2018-08-28 11:32:13 +0200

    Support for title in the notification, defaulting to 'Argus'

--- modules/pushover.py
+++ modules/pushover.py
@@ -2,12 +2,13 @@ import http.client
 import urllib
 
 
-def send_message(settings, message):
+def send_message(settings, message, title="Argus"):
     conn = http.client.HTTPSConnection("api.pushover.net:443")
     conn.request("POST", "/1/messages.json",
                  urllib.parse.urlencode({
                      "token": settings.PUSHOVER_APP_TOKEN,
                      "user": settings.PUSHOVER_USER_KEY,
                      "message": message,
+                     "title": title,
                  }), {"Content-type": "application/x-www-form-urlencoded"})
     conn.getresponse()
