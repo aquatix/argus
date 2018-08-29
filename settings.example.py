4fca91b28518b7baa9bed6c5efc619065f9973bb
Michiel Scholten <michiel@diginaut.net>
2018-08-29 16:34:02 +0200

    Example config

---
+++ settings.example.py
@@ -0,0 +1,15 @@
+
+APP_TOKEN = '<your_pushover_app_token>'
+USER_KEY = '<your_pushover_userkey>'
+
+FILESYSTEMS = {
+    'host001.example.com': [
+        ('/', 1),
+        ('/var/local', 5),
+        ('/home', 5),
+    ],
+    'host002': [
+        ('/', 5),
+        ('/stuff', 100),
+    ],
+}
