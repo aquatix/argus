96a675316fcbce3b434a4835bc842080dfaf4e53
Michiel Scholten <michiel@diginaut.net>
2025-08-26 17:13:13 +0200

    20240226: styling fixes

--- argus.py
+++ argus.py
@@ -1,19 +1,14 @@
-# -*- coding: utf-8 -*-
-"""
-    argus
-    ~~~~~~
+"""argus, a simple monitoring tool.
 
-    A simple monitoring tool
-
-    :copyright: (c) 2013-2023 by Michiel Scholten.
-    :license: BSD, see LICENSE for more details.
+:copyright: (c) 2013-2024 by Michiel Scholten.
+:license: BSD, see LICENSE for more details.
 """
 
 import click
 import tabulate
 
 import settings
-from modules import diskspacealarm, network, pushover, smartctl, telegram
+from modules import diskspacealarm, network, pushover, smartctl
 
 # get hostname for the current node
 HOSTNAME = network.get_local_hostname()
@@ -34,7 +29,7 @@ def check_diskspace():
 
 @cli.command()
 def rebooted():
-    """Sends a message about the node having rebooted"""
+    """Send a message about the node having rebooted."""
     message = (
         f'[{HOSTNAME}] Node has been rebooted. '
         + f'Local IP is {network.get_local_ip()}, public IP is {network.get_public_ip()}'
@@ -45,14 +40,14 @@ def rebooted():
 @cli.command()
 @click.argument('message')
 def send_message(message):
-    """Sends a message"""
+    """Send a message."""
     title = f'[{HOSTNAME}] Message'
     pushover.send_message(settings, message, title)
 
 
 @cli.command()
 def smartinfo():
-    """Checks disks on health status"""
+    """Check disks on health status."""
     devices = smartctl.get_devices()
     header, data = smartctl.get_information_on_drives(devices)
     for device in data:
@@ -65,6 +60,7 @@ def smartinfo():
 
 @cli.command()
 def node_info():
+    """Provide information about this computer."""
     print(f'Node {HOSTNAME}\nLocal IP is {network.get_local_ip()}\nPublic IP is {network.get_public_ip()}\n')
     diskspace_info  = diskspacealarm.check_diskspace(settings, HOSTNAME)
     if diskspace_info:
